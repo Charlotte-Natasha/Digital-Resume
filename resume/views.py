ab4d0da7f9443dda09c7b0cec192749fb125fcaf
Charlotte Natasha <natashaonyango97@gmail.com>
2022-08-08 16:11:42 +0200

    styled home page and details

--- resume/views.py
+++ resume/views.py
@@ -5,6 +5,23 @@ from django.views import generic
 from .forms import *
 
 # Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = "resume/index.html"
+
+def get_context_data(self, **kwargs):
+	context = get_context_data(**kwargs)
+	
+	testimonials = Testimonial.objects.filter(is_active=True)
+	certificates = Certificate.objects.filter(is_active=True)
+	blogs = Blog.objects.filter(is_active=True)
+	portfolio = Portfolio.objects.filter(is_active=True)
+	
+	context["testimonials"] = testimonials
+	context["certificates"] = certificates
+	context["blogs"] = blogs
+	context["portfolio"] = portfolio
+	return context
+
 # class ContactView(generic.FormView):
 
 #     template_name = 'resume/contact.html'
